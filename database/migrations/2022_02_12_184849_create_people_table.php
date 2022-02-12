9439dec7e04b9f5b7a2799aae9d65f9cdee5d3a0
Kelvin Chikezie <kevchike@gmail.com>
2022-02-12 19:57:38 +0100

    Add migration for people

---
+++ database/migrations/2022_02_12_184849_create_people_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('birth_year');
+            $table->string('eye_color');
+            $table->string('gender');
+            $table->string('hair_color');
+            $table->string('height');
+            $table->string('mass');
+            $table->string('skin_color');
+            $table->string('planet_id');
+            $table->datetime('created');
+            $table->datetime('edited');
+            $table->timestamps();
+
+            $table->foreign('planet_id')
+                  ->references('id')
+                  ->on('planets')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
