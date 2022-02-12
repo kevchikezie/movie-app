032d66c900c2c0b92b067902b7c1963752098acc
Kelvin Chikezie <kevchike@gmail.com>
2022-02-12 14:31:55 +0100

    Add migration for films

---
+++ database/migrations/2022_02_12_120149_create_films_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('episode_id');
+            $table->string('opening_crawl');
+            $table->string('director');
+            $table->string('producer');
+            $table->date('release_date');
+            $table->datetime('created');
+            $table->datetime('edited');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+};
