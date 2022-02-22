c89a1144e45b658dff34dfc916f096d71e713fd6
Kelvin Chikezie <kevchike@gmail.com>
2022-02-22 15:20:04 +0100

    Fetch people record

--- app/Http/Controllers/SwapiController.php
+++ app/Http/Controllers/SwapiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Film;
 use App\Models\Planet;
+use App\Models\People;
 use App\Mocks\SwapiMock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -22,11 +23,12 @@ class SwapiController extends Controller
     public function index()
     {
         // $this->getFilms();   
-        $this->getPlanets();   
+        // $this->getPlanets();   
+        $this->getPeople();
     }
 
     /**
-     * Fetch all films from swapi.dev save them to the database and also cache it
+     * Fetch all films from swapi.dev, save them to the database and also cache it
      * 
      * @return void
      */
@@ -70,7 +72,7 @@ class SwapiController extends Controller
     }
 
     /**
-     * Fetch all planets from swapi.dev 
+     * Fetch all planets from swapi.dev, save them to the database and also cache it 
      * 
      * @return void
      */
@@ -84,22 +86,22 @@ class SwapiController extends Controller
             $newPlanets = [];
             $data = [];
 
-            foreach ($planets['results'] as $film) {  
-                $created = explode('.', $film['created']);
+            foreach ($planets['results'] as $planet) {  
+                $created = explode('.', $planet['created']);
                 $created = $created[0];
 
-                $edited = explode('.', $film['edited']);
+                $edited = explode('.', $planet['edited']);
                 $edited = $edited[0];
 
-                $data['name'] = $film['name']; 
-                $data['diameter'] = $film['diameter'];
-                $data['rotation_period'] = $film['rotation_period'];
-                $data['orbital_period'] = $film['orbital_period'];
-                $data['gravity'] = $film['gravity'];
-                $data['population'] = $film['population'];
-                $data['climate'] = $film['climate'];
-                $data['terrain'] = $film['terrain'];
-                $data['surface_water'] = $film['surface_water'];
+                $data['name'] = $planet['name']; 
+                $data['diameter'] = $planet['diameter'];
+                $data['rotation_period'] = $planet['rotation_period'];
+                $data['orbital_period'] = $planet['orbital_period'];
+                $data['gravity'] = $planet['gravity'];
+                $data['population'] = $planet['population'];
+                $data['climate'] = $planet['climate'];
+                $data['terrain'] = $planet['terrain'];
+                $data['surface_water'] = $planet['surface_water'];
                 $data['created'] = $created;
                 $data['edited'] = $edited;
                 $data['created_at'] = now();
@@ -113,6 +115,59 @@ class SwapiController extends Controller
 
         Cache::remember('cachedPlanets', 1800, function () {
             return Planet::all();
+        });
+    }
+
+    /**
+     * Fetch all planets from swapi.dev, save them to the database and also cache it 
+     * 
+     * @return void
+     */
+    public function getPeople()
+    {
+        if (People::count() < 1) {
+            $url = $this->baseUrl . 'people';
+
+            $people = (config('app.env') === 'mock') ? SwapiMock::people() : Http::get($url);
+
+            $newpeople = [];
+            $data = [];
+
+            foreach ($people['results'] as $person) {  
+                $created = explode('.', $person['created']);
+                $created = $created[0];
+
+                $edited = explode('.', $person['edited']);
+                $edited = $edited[0];
+
+                $planet = explode('/', $person['homeworld']);
+                $planetId = $planet[count($planet) - 2];
+
+                $exists = Planet::find($planetId);
+                if ($exists) {
+                    $data['name'] = $person['name']; 
+                    $data['birth_year'] = $person['birth_year'];
+                    $data['eye_color'] = $person['eye_color'];
+                    $data['gender'] = $person['gender'];
+                    $data['hair_color'] = $person['hair_color'];
+                    $data['height'] = $person['height'];
+                    $data['mass'] = $person['mass'];
+                    $data['skin_color'] = $person['skin_color'];
+                    $data['planet_id'] = $planetId;
+                    $data['created'] = $created;
+                    $data['edited'] = $edited;
+                    $data['created_at'] = now();
+                    $data['updated_at'] = now();
+                    
+                    array_push($newpeople, $data);
+                }
+            }
+
+            People::insert($newpeople);
+        }
+
+        Cache::remember('cachedPeople', 1800, function () {
+            return People::all();
         });
     }
 
